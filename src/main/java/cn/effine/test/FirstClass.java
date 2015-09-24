03e2a708a022a09746c176f5520e494a48df1248
effine-mac <verphen@163.com>
2015-09-24 23:43:42 +0800

    update

--- src/main/java/cn/effine/test/FirstClass.java
+++ src/main/java/cn/effine/test/FirstClass.java
@@ -1,7 +1,7 @@
 package cn.effine.test;
 
 public class FirstClass {
-  public static void main(String[] args) {
-        System.out.println("Hello World!");
-            }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
