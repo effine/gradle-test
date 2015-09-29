a0cb6116e3f3723d36325c4d7e20ea1b7a0f9ef0
effine <verphen@163.com>
2015-09-29 18:21:55 +0800

    update

---
+++ src/test/java/cn/effine/test/Test.java
@@ -0,0 +1,16 @@
+/**
+ * @author effine
+ * @Date 2015年9月29日  下午5:33:39
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.test;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+}
